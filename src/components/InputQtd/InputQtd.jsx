a01f454c4629c19a62b66b9b29359b83976a0843
Carlos Bolzanell <carlosbolzanell@gmail.com>
2024-02-23 18:20:04 -0300

    Projetando

--- src/components/InputQtd/InputQtd.jsx
+++ src/components/InputQtd/InputQtd.jsx
@@ -1,12 +1,14 @@
 
-export default function InputQtd({valorQuantidade, value}) {
+export default function InputQtd({ valorQuantidade, value }) {
     return (
         <div className="flex flex-col items-center">
             <p className="font-montserrat text-xl text-cinza-escuro">Quantidade</p>
-            <div className="flex flex-row gap-5">
-                <button onClick={() => valorQuantidade(value == 0 ? 0 : value - 1)} className='border bg-verde w-7 h-6 rounded'>-</button>
-                <input className="border border-verde w-7 h-8 rounded" type="text" value={(value)? value : 0} disabled/>
-                <button onClick={() => valorQuantidade(value+1)} className="border bg-verde w-7 h-6 rounded">+</button>
+            <div className="flex flex-row items-center justify-center gap-5">
+                <button onClick={() => valorQuantidade(value == 0 ? 0 : value - 1)} className='bg-verde w-7 h-6 rounded'>-</button>
+                <div className="border border-verde w-8 h-8 rounded flex justify-center items-center">
+                    <p className="font-xl text-center" type="text" value={(value) ? value : 0} disabled>{value}</p>
+                </div>
+                <button onClick={() => valorQuantidade(value + 1)} className="bg-verde w-7 h-6 rounded">+</button>
             </div>
         </div>
     )
